bf909b8baa83eff734446522738d302e888667fb
Abjura <107774277+Abjuraa@users.noreply.github.com>
2024-11-14 21:19:49 -0500

    Abstraccion
    Conociendo como usar y para que sirve la abstraccion POO

---
+++ objetos/abstraccion.js
@@ -0,0 +1,110 @@
+class LearningPaths {
+    constructor({ name, cursos = [] }) {
+        this.name = name;
+        this.cursos = cursos;
+    }
+}
+class Cursos {
+    constructor(name, classes) {
+        this.name = name,
+            this.classes = classes
+    }
+}
+
+const html = new Cursos({
+    name: 'HTML',
+    classes: [
+        'HTML basico',
+        'HTML avanzado',
+        'HTML experto'
+    ]
+})
+
+const css = new Cursos({
+    name: 'CSS',
+    classes: [
+        'CSS basico',
+        'CSS avanzado',
+        'CSS experto'
+    ]
+})
+
+const javascript = new Cursos({
+    name: 'JAVASCRIPT',
+    classes: [
+        'JAVASCRIPT basico',
+        'JAVASCRIPT avanzado',
+        'JAVASCRIPT experto'
+    ]
+})
+const python = new Cursos({
+    name: 'PYTHON',
+    classes: [
+        'PYTHON basico',
+        'PYTHON avanzado',
+        'PYTHON experto'
+    ]
+})
+const rust = new Cursos({
+    name: 'RUST',
+    classes: [
+        'RUST basico',
+        'RUST avanzado',
+        'RUST experto'
+    ]
+})
+const sql = new Cursos({
+    name: 'SQL',
+    classes: [
+        'SQL basico',
+        'SQL avanzado',
+        'SQL experto'
+    ]
+})
+const unity = new Cursos({
+    name: 'UNITY',
+    classes: [
+        'UNITY basico',
+        'UNITY avanzado',
+        'UNITY experto'
+    ]
+})
+const unreal = new Cursos({
+    name: 'UNREAL',
+    classes: [
+        'UNREAL basico',
+        'UNREAL avanzado',
+        'UNREAL experto'
+    ]
+})
+const c = new Cursos({
+    name: 'C++',
+    classes: [
+        'C++ basico',
+        'C++ avanzado',
+        'C++ experto'
+    ]
+})
+
+const escuelaWeb = new LearningPaths({ name: 'Escuela web', cursos: [html, css, javascript] });
+const escuelaData = new LearningPaths({ name: 'Escuela de Data Science', cursos: [python, rust, sql] });
+const escuelaVgs = new LearningPaths({ name: 'Escuela de VideoJuegos', cursos: [unity, unreal, c] });
+
+class Estudiante {
+    constructor({ name, email, username, twitter = undefined, instagram = undefined, facebook = undefined, approvedCourses = [], learningPaths = [] }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = { twitter, instagram, facebook };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const abjura = new Estudiante({
+    name: 'juanito',
+    email: 'juanito.com',
+    username: 'Abjura',
+    twitter: 'abjura',
+    learningPaths: [escuelaWeb, escuelaVgs]
+})
--- objetos/script.js
+++ objetos/script.js
@@ -3,7 +3,7 @@ const natalia = {
     nombre: 'Natalia',
     edad: 20,
     cursosAprobados: ['angular', 'html', 'css'],
-    aprobarCursos(nuevoCurso){
+    aprobarCursos(nuevoCurso) {
         this.cursosAprobados.push(nuevoCurso)
     },
 }
@@ -11,11 +11,11 @@ const natalia = {
 //prototipos 
 function Student(name, age, cursosAprobados) {
     this.name = name,
-    this.age = age,
-    this.cursosAprobados = cursosAprobados
+        this.age = age,
+        this.cursosAprobados = cursosAprobados
 }
 
-Student.prototype.aprobarCurso = function(nuevoCurso){
+Student.prototype.aprobarCurso = function (nuevoCurso) {
     this.cursosAprobados.push(nuevoCurso)
 }
 
@@ -23,48 +23,59 @@ const juanita = new Student('juanita', 20, ['javascript', 'react', 'vue'])
 
 //prototipos con la sintaxis de clases
 
-class Student2{
-    constructor({name, age, cursosAprobados = [], email}){
+class Student2 {
+    constructor({ name, age, cursosAprobados = [], email }) {
         this.name = name,
-        this.age = age,
-        this.email = email,
-        this.cursosAprobados = cursosAprobados
+            this.age = age,
+            this.email = email,
+            this.cursosAprobados = cursosAprobados
     }
-    aprobarCurso(nuevoCursito){
+    aprobarCurso(nuevoCursito) {
         this.cursosAprobados.push(nuevoCursito)
     }
 }
 
-const miguel = new Student2({name:'miguel', age:20, email: 'miguel.com'})   
+const miguel = new Student2({ name: 'miguel', age: 20, email: 'miguel.com' })
 
-//prueba
+//prueba Banda de rock
 
-class LearningPaths{
-    constructor({name, cursos = []}){
-        this.name = name;
-        this.cursos = cursos;
+class Banda {
+    constructor({ nombre, generos = [] }) {
+        this.nombre = nombre;
+        this.generos = generos;
+        this.integrantes = [];
+    }
+    agregarIntegrante(integranteNuevo) {
+        //Verificar si el integrante ya existe
+        const existente = this.integrantes.find(integrante => integrante.instrumento === integranteNuevo.instrumento);
+
+        if (existente) {
+            //Si el integrante ya existe
+            console.log(`El integrante con el instrumento ${integranteNuevo.instrumento} ya existe`);
+        } else {
+            //Si el integrante no existe
+            this.integrantes.push(integranteNuevo);
+            console.log(`${integranteNuevo.nombre} ha sido integrado a la banda con el instrumento ${integranteNuevo.instrumento}`);
+        }
     }
 }
 
-const escuelaWeb = new LearningPaths({name: 'Escuela web', cursos: ["HTML", 'CSS', 'JavaScript']});
-const escuelaData = new LearningPaths({name: 'Escuela de Data Science', cursos: ["Python", 'R', 'SQL']});
-const escuelaVgs = new LearningPaths({name: 'Escuela de VideoJuegos', cursos: ["Unity", 'Unreal', 'C++']});
-
-class Estudiante{
-    constructor({name, email, username, twitter = undefined, instagram = undefined, facebook = undefined, approvedCourses = [], learningPaths = []}){
-        this.name = name;
-        this.email = email;
-        this.username = username;
-        this.socialMedia = { twitter, instagram, facebook };
-        this.approvedCourses = approvedCourses;
-        this.learningPaths = learningPaths;
+//Crear clase Integrante
+class Integrante {
+    constructor({ nombre, instrumento }) {
+        this.nombre = nombre;
+        this.instrumento = instrumento;
     }
 }
 
-const juanito = new Estudiante({
-    name: 'juanito',
-    email: 'juanito.com',
-    username: 'Abjura',
-    twitter: 'abjura',
-    learningPaths: [escuelaWeb, escuelaVgs]
-})
+const banda = new Banda({ nombre: 'Metallica', generos: ['Metal', 'Rock'] });
+
+banda.agregarIntegrante(new Integrante({ nombre: 'Bryam', instrumento: 'guitarra' }))
+banda.agregarIntegrante(new Integrante({ nombre: 'kevin', instrumento: 'bateria' }))
+banda.agregarIntegrante(new Integrante({ nombre: 'lincon', instrumento: 'flauta' }))
+banda.agregarIntegrante(new Integrante({ nombre: 'Bryam', instrumento: 'piano' }))
+banda.agregarIntegrante(new Integrante({ nombre: 'maicol', instrumento: 'guitarra' }))
+
+
+
+
